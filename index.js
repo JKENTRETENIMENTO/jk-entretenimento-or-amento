4f2fe1a7ed63da2c97ff4abb18827be25a8f2584
JKENTRETENIMENTO <jkentretenimento4@gmail.com>
2025-06-25 14:28:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import JKEntretenimentoApp from './JKEntretenimentoApp';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>JK Entretenimento</title>
+      </Head>
+      <JKEntretenimentoApp />
+    </>
+  );
+}
